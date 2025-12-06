ad1a792b2bd09a1e5ca0db7fdb4ae5212a99393a
大塚優輝 <2250038@ecc.ac.jp>
2025-12-06 15:45:06 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -10,22 +10,11 @@ $sql ="
 
 $stmt = $pdo->query($sql);
 $product = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-
 ?>
+
 <?php include 'parts/header.php'; ?>
 <main>
-
-    <div class="menu-items">
-        <span class="item" data-type="1000">ケーキ</span>
-        <span class="item" data-type="2000">タルト</span>
-        <span class="item" data-type="3000">パフェ</span>
-        <span class="item" data-type="4000">かき氷</span>
-        <span class="item" data-type="5000">その他</span>
-        <span class="item" data-type="6000">ドリンク</span>
-    </div>
-
+    <?php include 'parts/menu-items.php'; ?>
     <div class="products">
         <?php foreach ($product as $p): ?>
             <?php include 'parts/productCard.php'; ?>
