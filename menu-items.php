0232021eff81497450e9a8a165870307f752b60a
大塚優輝 <2250038@ecc.ac.jp>
2026-01-13 13:43:39 +0900

    Delete menu-items.php

--- menu-items.php
+++
@@ -1,8 +0,0 @@
-<div class="menu-items">
-    <span class="item" data-type="1000">ケーキ</span>
-    <span class="item" data-type="2000">タルト</span>
-    <span class="item" data-type="3000">パフェ</span>
-    <span class="item" data-type="4000">かき氷</span>
-    <span class="item" data-type="5000">その他</span>
-    <span class="item" data-type="6000">ドリンク</span>
-</div>
