f286eae5454583bc048cd8f42a31e0e89faa334a
大塚優輝 <2250038@ecc.ac.jp>
2026-05-17 23:21:19 +0900

    ファビコン追加

--- login/login.php
+++ login/login.php
@@ -25,6 +25,7 @@ if (isset($_SESSION['user'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/login.css">
+  <link rel="icon" href="../images/favicon.ico" type="image/x-icon">
   <title>ログインページ</title>
 </head>
 <body>
